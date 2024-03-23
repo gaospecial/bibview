18c10147a99cb1b41ab3466c87053b8f3b5f4de8
xingyh2568 <1517003428@qq.com>
2024-03-23 22:30:45 +0800

    “3/23admin-web-controller“

--- RuoYi-master/ruoyi-admin/src/main/java/com/ruoyi/web/controller/monitor/SysLogininforController.java
+++ RuoYi-master/ruoyi-admin/src/main/java/com/ruoyi/web/controller/monitor/SysLogininforController.java
@@ -1,14 +1,15 @@
 package com.ruoyi.web.controller.monitor;
 
 import java.util.List;
-import com.ruoyi.framework.shiro.service.SysPasswordService;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
@@ -23,28 +24,15 @@ import com.ruoyi.system.service.ISysLogininforService;
  * 
  * @author ruoyi
  */
-@Controller
+@RestController
 @RequestMapping("/monitor/logininfor")
 public class SysLogininforController extends BaseController
 {
-    private String prefix = "monitor/logininfor";
-
     @Autowired
     private ISysLogininforService logininforService;
 
-    @Autowired
-    private SysPasswordService passwordService;
-
-    @RequiresPermissions("monitor:logininfor:view")
-    @GetMapping()
-    public String logininfor()
-    {
-        return prefix + "/logininfor";
-    }
-
-    @RequiresPermissions("monitor:logininfor:list")
-    @PostMapping("/list")
-    @ResponseBody
+    @PreAuthorize("@ss.hasPermi('monitor:logininfor:list')")
+    @GetMapping("/list")
     public TableDataInfo list(SysLogininfor logininfor)
     {
         startPage();
@@ -53,42 +41,29 @@ public class SysLogininforController extends BaseController
     }
 
     @Log(title = "登录日志", businessType = BusinessType.EXPORT)
-    @RequiresPermissions("monitor:logininfor:export")
+    @PreAuthorize("@ss.hasPermi('monitor:logininfor:export')")
     @PostMapping("/export")
-    @ResponseBody
-    public AjaxResult export(SysLogininfor logininfor)
+    public void export(HttpServletResponse response, SysLogininfor logininfor)
     {
         List<SysLogininfor> list = logininforService.selectLogininforList(logininfor);
         ExcelUtil<SysLogininfor> util = new ExcelUtil<SysLogininfor>(SysLogininfor.class);
-        return util.exportExcel(list, "登录日志");
+        util.exportExcel(response, list, "登录日志");
     }
 
-    @RequiresPermissions("monitor:logininfor:remove")
+    @PreAuthorize("@ss.hasPermi('monitor:logininfor:remove')")
     @Log(title = "登录日志", businessType = BusinessType.DELETE)
-    @PostMapping("/remove")
-    @ResponseBody
-    public AjaxResult remove(String ids)
+    @DeleteMapping("/{infoIds}")
+    public AjaxResult remove(@PathVariable Long[] infoIds)
     {
-        return toAjax(logininforService.deleteLogininforByIds(ids));
+        return toAjax(logininforService.deleteLogininforByIds(infoIds));
     }
-    
-    @RequiresPermissions("monitor:logininfor:remove")
+
+    @PreAuthorize("@ss.hasPermi('monitor:logininfor:remove')")
     @Log(title = "登录日志", businessType = BusinessType.CLEAN)
-    @PostMapping("/clean")
-    @ResponseBody
+    @DeleteMapping("/clean")
     public AjaxResult clean()
     {
         logininforService.cleanLogininfor();
-        return success();
-    }
-
-    @RequiresPermissions("monitor:logininfor:unlock")
-    @Log(title = "账户解锁", businessType = BusinessType.OTHER)
-    @PostMapping("/unlock")
-    @ResponseBody
-    public AjaxResult unlock(String loginName)
-    {
-        passwordService.clearLoginRecordCache(loginName);
-        return success();
+        return AjaxResult.success();
     }
 }
--- RuoYi-master/ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysProfileController.java
+++ RuoYi-master/ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysProfileController.java
@@ -1,28 +1,30 @@
 package com.ruoyi.web.controller.system;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.IOException;
+
+import com.ruoyi.common.constant.Constants;
+import com.ruoyi.common.utils.file.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.config.RuoYiConfig;
+import com.ruoyi.common.constant.UserConstants;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.core.domain.entity.SysUser;
+import com.ruoyi.common.core.domain.model.LoginUser;
 import com.ruoyi.common.enums.BusinessType;
-import com.ruoyi.common.utils.DateUtils;
-import com.ruoyi.common.utils.ShiroUtils;
+import com.ruoyi.common.utils.SecurityUtils;
 import com.ruoyi.common.utils.StringUtils;
 import com.ruoyi.common.utils.file.FileUploadUtils;
-import com.ruoyi.common.utils.file.MimeTypeUtils;
-import com.ruoyi.framework.shiro.service.SysPasswordService;
+import com.ruoyi.framework.web.service.TokenService;
 import com.ruoyi.system.service.ISysUserService;
 
 /**
@@ -30,152 +32,118 @@ import com.ruoyi.system.service.ISysUserService;
  * 
  * @author ruoyi
  */
-@Controller
+@RestController
 @RequestMapping("/system/user/profile")
 public class SysProfileController extends BaseController
 {
-    private static final Logger log = LoggerFactory.getLogger(SysProfileController.class);
-
-    private String prefix = "system/user/profile";
-
     @Autowired
     private ISysUserService userService;
-    
+
     @Autowired
-    private SysPasswordService passwordService;
+    private TokenService tokenService;
 
     /**
      * 个人信息
      */
-    @GetMapping()
-    public String profile(ModelMap mmap)
+    @GetMapping
+    public AjaxResult profile()
     {
-        SysUser user = getSysUser();
-        mmap.put("user", user);
-        mmap.put("roleGroup", userService.selectUserRoleGroup(user.getUserId()));
-        mmap.put("postGroup", userService.selectUserPostGroup(user.getUserId()));
-        return prefix + "/profile";
-    }
-
-    @GetMapping("/checkPassword")
-    @ResponseBody
-    public boolean checkPassword(String password)
-    {
-        SysUser user = getSysUser();
-        return passwordService.matches(user, password);
-    }
-
-    @GetMapping("/resetPwd")
-    public String resetPwd(ModelMap mmap)
-    {
-        SysUser user = getSysUser();
-        mmap.put("user", userService.selectUserById(user.getUserId()));
-        return prefix + "/resetPwd";
-    }
-
-    @Log(title = "重置密码", businessType = BusinessType.UPDATE)
-    @PostMapping("/resetPwd")
-    @ResponseBody
-    public AjaxResult resetPwd(String oldPassword, String newPassword)
-    {
-        SysUser user = getSysUser();
-        if (!passwordService.matches(user, oldPassword))
-        {
-            return error("修改密码失败，旧密码错误");
-        }
-        if (passwordService.matches(user, newPassword))
-        {
-            return error("新密码不能与旧密码相同");
-        }
-        user.setSalt(ShiroUtils.randomSalt());
-        user.setPassword(passwordService.encryptPassword(user.getLoginName(), newPassword, user.getSalt()));
-        user.setPwdUpdateDate(DateUtils.getNowDate());
-        if (userService.resetUserPwd(user) > 0)
-        {
-            setSysUser(userService.selectUserById(user.getUserId()));
-            return success();
-        }
-        return error("修改密码异常，请联系管理员");
-    }
-
-    /**
-     * 修改用户
-     */
-    @GetMapping("/edit")
-    public String edit(ModelMap mmap)
-    {
-        SysUser user = getSysUser();
-        mmap.put("user", userService.selectUserById(user.getUserId()));
-        return prefix + "/edit";
-    }
-
-    /**
-     * 修改头像
-     */
-    @GetMapping("/avatar")
-    public String avatar(ModelMap mmap)
-    {
-        SysUser user = getSysUser();
-        mmap.put("user", userService.selectUserById(user.getUserId()));
-        return prefix + "/avatar";
+        LoginUser loginUser = getLoginUser();
+        SysUser user = loginUser.getUser();
+        AjaxResult ajax = AjaxResult.success(user);
+        ajax.put("roleGroup", userService.selectUserRoleGroup(loginUser.getUsername()));
+        ajax.put("postGroup", userService.selectUserPostGroup(loginUser.getUsername()));
+        return ajax;
     }
 
     /**
      * 修改用户
      */
     @Log(title = "个人信息", businessType = BusinessType.UPDATE)
-    @PostMapping("/update")
-    @ResponseBody
-    public AjaxResult update(SysUser user)
+    @PutMapping
+    public AjaxResult updateProfile(@RequestBody SysUser user)
     {
-        SysUser currentUser = getSysUser();
-        currentUser.setUserName(user.getUserName());
-        currentUser.setEmail(user.getEmail());
-        currentUser.setPhonenumber(user.getPhonenumber());
-        currentUser.setSex(user.getSex());
-        if (StringUtils.isNotEmpty(user.getPhonenumber()) && !userService.checkPhoneUnique(currentUser))
+        LoginUser loginUser = getLoginUser();
+        SysUser sysUser = loginUser.getUser();
+        user.setUserName(sysUser.getUserName());
+        if (StringUtils.isNotEmpty(user.getPhonenumber())
+                && UserConstants.NOT_UNIQUE.equals(userService.checkPhoneUnique(user)))
         {
-            return error("修改用户'" + currentUser.getLoginName() + "'失败，手机号码已存在");
+            return AjaxResult.error("修改用户'" + user.getUserName() + "'失败，手机号码已存在");
         }
-        else if (StringUtils.isNotEmpty(user.getEmail()) && !userService.checkEmailUnique(currentUser))
+        if (StringUtils.isNotEmpty(user.getEmail())
+                && UserConstants.NOT_UNIQUE.equals(userService.checkEmailUnique(user)))
         {
-            return error("修改用户'" + currentUser.getLoginName() + "'失败，邮箱账号已存在");
+            return AjaxResult.error("修改用户'" + user.getUserName() + "'失败，邮箱账号已存在");
         }
-        if (userService.updateUserInfo(currentUser) > 0)
+        user.setUserId(sysUser.getUserId());
+        user.setPassword(null);
+        if (userService.updateUserProfile(user) > 0)
         {
-            setSysUser(userService.selectUserById(currentUser.getUserId()));
-            return success();
+            // 更新缓存用户信息
+            sysUser.setNickName(user.getNickName());
+            sysUser.setPhonenumber(user.getPhonenumber());
+            sysUser.setEmail(user.getEmail());
+            sysUser.setSex(user.getSex());
+            tokenService.setLoginUser(loginUser);
+            return AjaxResult.success();
         }
-        return error();
+        return AjaxResult.error("修改个人信息异常，请联系管理员");
     }
 
     /**
-     * 保存头像
+     * 重置密码
      */
     @Log(title = "个人信息", businessType = BusinessType.UPDATE)
-    @PostMapping("/updateAvatar")
-    @ResponseBody
-    public AjaxResult updateAvatar(@RequestParam("avatarfile") MultipartFile file)
+    @PutMapping("/updatePwd")
+    public AjaxResult updatePwd(String oldPassword, String newPassword)
     {
-        SysUser currentUser = getSysUser();
-        try
+        LoginUser loginUser = getLoginUser();
+        String userName = loginUser.getUsername();
+        String password = loginUser.getPassword();
+        if (!SecurityUtils.matchesPassword(oldPassword, password))
         {
-            if (!file.isEmpty())
+            return AjaxResult.error("修改密码失败，旧密码错误");
+        }
+        if (SecurityUtils.matchesPassword(newPassword, password))
+        {
+            return AjaxResult.error("新密码不能与旧密码相同");
+        }
+        if (userService.resetUserPwd(userName, SecurityUtils.encryptPassword(newPassword)) > 0)
+        {
+            // 更新缓存用户密码
+            loginUser.getUser().setPassword(SecurityUtils.encryptPassword(newPassword));
+            tokenService.setLoginUser(loginUser);
+            return AjaxResult.success();
+        }
+        return AjaxResult.error("修改密码异常，请联系管理员");
+    }
+
+    /**
+     * 头像上传
+     */
+    @Log(title = "用户头像", businessType = BusinessType.UPDATE)
+    @PostMapping("/avatar")
+    public AjaxResult avatar(@RequestParam("avatarfile") MultipartFile file) throws IOException
+    {
+        if (!file.isEmpty())
+        {
+            LoginUser loginUser = getLoginUser();
+            String avatar = FileUploadUtils.upload(RuoYiConfig.getAvatarPath(), file);
+            /*2021-12-29 新增删除原头像*/
+            String filePath = RuoYiConfig.getProfile() + StringUtils.substringAfter(loginUser.getUser().getAvatar(), Constants.RESOURCE_PREFIX);
+            FileUtils.deleteFile(filePath);
+            /*结束*/
+            if (userService.updateUserAvatar(loginUser.getUsername(), avatar))
             {
-                String avatar = FileUploadUtils.upload(RuoYiConfig.getAvatarPath(), file, MimeTypeUtils.IMAGE_EXTENSION);
-                currentUser.setAvatar(avatar);
-                if (userService.updateUserInfo(currentUser) > 0)
-                {
-                    setSysUser(userService.selectUserById(currentUser.getUserId()));
-                    return success();
-                }
+                AjaxResult ajax = AjaxResult.success();
+                ajax.put("imgUrl", avatar);
+                // 更新缓存用户头像
+                loginUser.getUser().setAvatar(avatar);
+                tokenService.setLoginUser(loginUser);
+                return ajax;
             }
-            return error();
         }
-        catch (Exception e)
-        {
-            log.error("修改头像失败！", e);
-            return error(e.getMessage());
-        }
+        return AjaxResult.error("上传图片异常，请联系管理员");
     }
 }
